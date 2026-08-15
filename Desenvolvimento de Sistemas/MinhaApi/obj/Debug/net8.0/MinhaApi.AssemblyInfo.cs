89789c5a635b9ef8114d15d7249e8a63bc28dccd
Gabriel Rodrigues <rgabrielsantos.ti@outlook.com>
2026-08-15 19:59:28 -0300

    atualizando e configurando essa bomba

--- Desenvolvimento de Sistemas/MinhaApi/obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
+++ Desenvolvimento de Sistemas/MinhaApi/obj/Debug/net8.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02fb2a7dd787d3c0b179b4138f4e7fb5677ce966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96abe060892d21e2aa70bb34e79444983dc00080")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
